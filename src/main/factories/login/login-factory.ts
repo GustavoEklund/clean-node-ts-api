e3edd547f732675e916d76af6bf3b99acb7a54e4
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-28 16:40:28 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,13 +1,13 @@
-import env from '@/main/config/env'
-import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
+import env from '../../config/env'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { makeLoginValidation } from './login-validation-factory'
 import { Controller } from '@/presentation/protocols'
-import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
-import { LoginController } from '@/presentation/controllers/login/login-controller'
-import { LogMongoRepository } from '@/infra/db/mongodb/log/log-mongo-repository'
-import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { LoginController } from '../../../presentation/controllers/login/login-controller'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 
 export const makeLoginController = (): Controller => {
   const salt = 12
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
-import { makeSignupController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeSignupController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
