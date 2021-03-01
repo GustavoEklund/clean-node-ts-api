547f2f26fc88e815277cb8ed6cdc441efd7a68cc
Gustavo Eklund <gustavo04616@gmail.com>
2021-02-28 21:16:18 -0300

    chore: change jwtSecret env name

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.PORT || 'secret==666AX'
+  jwtSecret: process.env.JWT_SECRET || 'secret==666AX'
 }
