993ee5d6f04185309247e7cb7acb88e28162c9d4
Gustavo Eklund <gustavo04616@gmail.com>
2021-03-02 16:34:57 -0300

    fix: add AccountMongoRepository on DbAddAccount composition

--- src/main/factories/decorators/log-controller-decorator-factory.ts
+++ src/main/factories/decorators/log-controller-decorator-factory.ts
@@ -1,4 +1,3 @@
-import env from '../../../config/env'
 import { Controller } from '@/presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
